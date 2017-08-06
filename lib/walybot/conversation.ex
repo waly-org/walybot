e13bd320679ba41a5c717b0cace30a8aed22f218
Michael Ries <michael@riesd.com>
2017-08-05 22:35:02 -0600

    create a conversation for each update created

--- lib/walybot/conversation.ex
+++ lib/walybot/conversation.ex
@@ -1,13 +1,19 @@
 defmodule Walybot.Conversation do
   use GenServer
   require Logger
+  alias Walybot.Ecto.Conversation
 
   def start_link(name, conversation_id) do
     GenServer.start_link(__MODULE__, conversation_id, name: name)
   end
 
   def init(conversation_id) do
-    {:ok, %{conversation_id: conversation_id}}
+    {:ok, conversation} = Conversation.first_or_create(conversation_id)
+    state = %{
+      conversation: conversation,
+      conversation_id: conversation_id,
+    }
+    {:ok, state}
   end
 
   def handle_call({:update, update}, _from, state) do
--- lib/walybot/ecto/conversation.ex
+++ lib/walybot/ecto/conversation.ex
@@ -1,6 +1,7 @@
 defmodule Walybot.Ecto.Conversation do
   use Ecto.Schema
-  import Ecto.Changeset
+  import Ecto.{Changeset,Query}
+  alias Walybot.Ecto.Repo
 
   schema "conversations" do
     field :name, :string
@@ -12,7 +13,14 @@ defmodule Walybot.Ecto.Conversation do
   def changeset(params, conversation \\ %__MODULE__{}) do
     conversation
     |> cast(params, [:name, :needs_translation, :telegram_id])
-    |> validate_required([:name, :needs_translation, :telegram_id])
+    |> validate_required([:needs_translation, :telegram_id])
     |> unique_constraint(:telegram_id)
+  end
+
+  def first_or_create(telegram_id) do
+    case __MODULE__ |> where(telegram_id: ^telegram_id) |> Repo.one do
+      nil -> %{telegram_id: telegram_id} |> changeset |> Repo.insert
+      conversation -> {:ok, conversation}
+    end
   end
 end
