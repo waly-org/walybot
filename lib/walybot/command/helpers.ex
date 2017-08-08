9fae78ee186a6865005a95371349f4d7a13b863e
Michael Ries <michael@riesd.com>
2017-08-08 08:04:29 -0600

    refactor helpers

--- lib/walybot/command/helpers.ex
+++ lib/walybot/command/helpers.ex
@@ -2,10 +2,22 @@ defmodule Walybot.Command.Helpers do
   import Ecto.Query
   alias Walybot.Ecto.{Repo,User}
 
+  def custom_keyboard(button_pairs) do
+    buttons = button_pairs
+              |> Enum.map(fn({key, label}) -> %{text: label, callback_data: key} end)
+              |> group_buttons_into_rows_of_three
+    %{
+      reply_markup: %{
+        inline_keyboard: buttons
+      }
+    }
+  end
+
   def handle_callback_error(query, fun) do
     case fun.() do
       :ok -> :ok
       {:ok, _data} -> :ok
+      {:context, new_context} -> {:context, new_context}
       {:error, reason} ->
         reason = error_message(reason)
         case Telegram.Bot.edit_message(query, "😢 #{reason}") do
@@ -19,6 +31,7 @@ defmodule Walybot.Command.Helpers do
     case fun.() do
       :ok -> :ok
       {:ok, _data} -> :ok
+      {:context, new_context} -> {:context, new_context}
       {:error, reason} ->
         reason = error_message(reason)
         case Telegram.Bot.send_message(update, "😢 #{reason}") do
@@ -43,6 +56,14 @@ defmodule Walybot.Command.Helpers do
   end
   def error_message(reason), do: reason
 
+  def group_buttons_into_rows_of_three(buttons) do
+    buttons
+    |> Enum.chunk(3, 3, [nil, nil, nil])
+    |> Enum.map(fn(buttons) ->
+      Enum.filter(buttons, &( !is_nil(&1) ))
+    end)
+  end
+
   def lookup_translator_by_id(str) when is_binary(str), do: str |> String.to_integer |> lookup_translator_by_id
   def lookup_translator_by_id(id) do
     import Ecto.Query
@@ -59,8 +80,7 @@ defmodule Walybot.Command.Helpers do
     end
   end
 
-  def parse_username(cmd, text) do
-    prefix = "#{cmd} @"
+  def parse_username(prefix, text) do
     case String.starts_with?(text, prefix) do
       true ->
         prefix_length = String.length(prefix)
@@ -69,17 +89,14 @@ defmodule Walybot.Command.Helpers do
           [username] -> {:ok, username}
           _ -> {:error, "I din't understand that username, please make sure it is a single username like @example"}
         end
-      false -> {:error, "please provide a usernme: #{cmd} @example"}
+      false -> {:error, "please provide a username: #{prefix}example"}
     end
   end
 
   def show_translator_list_keyboard(update, translators, prompt) do
     translator_buttons = translators
                          |> Enum.map(fn(t) -> %{text: "@#{t.username}", callback_data: Integer.to_string(t.id)} end)
-                         |> Enum.chunk(3, 3, [nil, nil, nil])
-                         |> Enum.map(fn(buttons) ->
-                           Enum.filter(buttons, &( !is_nil(&1) ))
-                         end)
+                         |> group_buttons_into_rows_of_three
 
     case translator_buttons do
       [] -> Telegram.Bot.send_message(update, "no translators available 🎉🤖")
