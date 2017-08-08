0070ddc718fdc415351e96978a140baa9ff80ae6
Michael Ries <michael@riesd.com>
2017-08-08 08:01:51 -0600

    handle command and callback errors in the switchboard to avoid boilerplate
    Also support routing to an aribtrary module with an abritrary argument based on the :expecting key in the conversation context

--- lib/walybot/switchboard.ex
+++ lib/walybot/switchboard.ex
@@ -1,19 +1,27 @@
 defmodule Walybot.Switchboard do
   require Logger
 
-  def update(%{"message" => %{"text" => text}}=update, conversation_context), do: text_message(text, update, conversation_context)
-  def update(%{"callback_query" => query}, conversation_context), do: callback_query(query, conversation_context)
+  def update(%{"message" => %{"text" => text}}=update, conversation_context) do
+    Walybot.Command.Helpers.handle_command_error(update, fn ->
+      text_message(text, update, conversation_context)
+    end)
+  end
+  def update(%{"callback_query" => query}, conversation_context) do
+    Walybot.Command.Helpers.handle_callback_error(query, fn ->
+      callback_query(query, conversation_context)
+    end)
+  end
   def update(%{"message" => %{"new_chat_member" => _}}, _conversation_context), do: :ok
   def update(%{"message" => %{"left_chat_member" => _}}, _conversation_context), do: :ok
   def update(update, _conversation_context) do
     Logger.info "not sure what type of message this is #{inspect update}"
   end
 
-  defp callback_query(%{"message" => %{"text" => "admin"<>_}}=query, %{user: user}), do: Walybot.Command.Admin.callback(query, user)
+  defp callback_query(%{"message" => %{"text" => "admin"<>_}}=query, context), do: Walybot.Command.Admin.callback(query, context)
   defp callback_query(%{"message" => %{"text" => "activate"<>_}}=query, _conversation_context), do: Walybot.Command.ActivateTranslator.callback(query)
   defp callback_query(%{"message" => %{"text" => "deactivate"<>_}}=query, _conversation_context), do: Walybot.Command.DeactivateTranslator.callback(query)
-  defp callback_query(query, _conversation_context) do
-    Logger.info "unhandled callback query: #{inspect query}"
+  defp callback_query(query, conversation_context) do
+    Logger.info "unhandled callback query: #{inspect query} (#{inspect conversation_context})"
     :ok
   end
 
@@ -23,6 +31,7 @@ defmodule Walybot.Switchboard do
   defp text_message("/deactivate"<>_=command, update, _conversation_context), do: Walybot.Command.DeactivateTranslator.command(command, update)
   defp text_message("/list"<>_, update, _conversation_context), do: Walybot.Command.ListTranslators.command("/list", update)
   defp text_message("/translate"<>_, update, _conversation_context), do: Walybot.Command.GetTranslation.command("/translate", update)
+  defp text_message(_text, update, %{expecting: {module, arg}}=context), do: apply(module, :expecting, [arg, update, context])
   defp text_message(_, %{"message" => %{"chat" => %{"type" => "private"}}}=update, _conversation_context), do: Walybot.Command.ProvideTranslation.command("", update)
   defp text_message(_, %{"message" => %{"chat" => %{"type" => "group"}}}=update, _conversation_context) do
     Walybot.Conversations.queue_for_translation(update)
